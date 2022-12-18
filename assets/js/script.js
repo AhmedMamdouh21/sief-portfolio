72ccd0d1eba79d816ea7bbfdf35a7eabba945ebd
Ahmed Mamdouh <ahmed.mamdouh2130@gmail.com>
2022-12-18 03:05:12 +0200

    video-works

--- assets/js/script.js
+++ assets/js/script.js
@@ -107,6 +107,10 @@ if ($(".backToTop").length) {
   });
 }
 
+if ($("select").length) {
+  $("select").selectpicker();
+}
+
 if ($(".container-filter").length) {
   var $filterSelect = $("#FilterSelect"),
     // $sortSelect = $("#SortSelect"),
@@ -125,6 +129,27 @@ if ($(".container-filter").length) {
   });
 }
 
-if ($("select").length) {
-  $("select").selectpicker();
+if ($(".swiper-see-also").length) {
+  let swiperSeeAlso = new Swiper(".swiper-see-also", {
+    speed: 800,
+    autoplay: {
+      disableOnInteraction: false,
+      delay: 2000,
+    },
+    loop: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1.5,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2.5,
+        spaceBetween: 30,
+      },
+      1024: {
+        slidesPerView: 3.5,
+        spaceBetween: 20,
+      },
+    },
+  });
 }
