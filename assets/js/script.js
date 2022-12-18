b78ca4cabb9e8c5409d7206baf69b7e3c4e6dd91
Ahmed Mamdouh <ahmed.mamdouh2130@gmail.com>
2022-12-18 22:33:14 +0200

    works & Cursor

--- assets/js/script.js
+++ assets/js/script.js
@@ -140,7 +140,7 @@ if ($(".swiper-see-also").length) {
     breakpoints: {
       0: {
         slidesPerView: 1.5,
-        spaceBetween: 20,
+        spaceBetween: 10,
       },
       768: {
         slidesPerView: 2.5,
@@ -152,4 +152,195 @@ if ($(".swiper-see-also").length) {
       },
     },
   });
+}
+
+if ($(".inner-works").length) {
+  var swiperThumbs = new Swiper(".swiper_thumbs", {
+    spaceBetween: 15,
+    slidesPerView: 3,
+    // freeMode: true,
+    // mousewheel: true,
+    centeredSlides: true,
+    slideToClickedSlide: true,
+    loop: true,
+    loopedSlides: 4,
+    // watchSlidesProgress: true,
+    direction: "vertical",
+    breakpoints: {
+      0: {
+        direction: "horizontal",
+        spaceBetween: 5,
+      },
+      1200: {
+        direction: "vertical",
+        spaceBetween: 15,
+      },
+    },
+  });
+  var swiperImages = new Swiper(".swiper_images", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 1,
+    loopedSlides: 4,
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true,
+    },
+    // speed: 800,
+    // autoplay: {
+    //   disableOnInteraction: false,
+    //   delay: 2000,
+    // },
+    // thumbs: {
+    //   swiper: swiperThumbs,
+    // },
+  });
+
+  swiperImages.controller.control = swiperThumbs;
+  swiperThumbs.controller.control = swiperImages;
+}
+
+// Cursor
+if ($(".cursor-dot").length) {
+  var cursorCustom = {
+    delay: 8,
+    _x: 0,
+    _y: 0,
+    endX: window.innerWidth / 2,
+    endY: window.innerHeight / 2,
+    cursorVisible: true,
+    cursorEnlarged: false,
+    $dot: document.querySelector(".cursor-dot"),
+    $outline: document.querySelector(".cursor-dot-outline"),
+
+    init: function () {
+      // Set up element sizes
+      this.dotSize = this.$dot.offsetWidth;
+      this.outlineSize = this.$outline.offsetWidth;
+
+      this.setupEventListeners();
+      this.animateDotOutline();
+    },
+
+    //     updateCursor: function(e) {
+    //         var self = this;
+
+    //         console.log(e)
+
+    //         // Show the cursor
+    //         self.cursorVisible = true;
+    //         self.toggleCursorVisibility();
+
+    //         // Position the dot
+    //         self.endX = e.pageX;
+    //         self.endY = e.pageY;
+    //         self.$dot.style.top = self.endY + 'px';
+    //         self.$dot.style.left = self.endX + 'px';
+    //     },
+
+    setupEventListeners: function () {
+      var self = this;
+
+      // Anchor hovering
+      document.querySelectorAll("a, button, .nav-link").forEach(function (el) {
+        el.addEventListener("mouseover", function () {
+          self.cursorEnlarged = true;
+          self.toggleCursorSize();
+        });
+        el.addEventListener("mouseout", function () {
+          self.cursorEnlarged = false;
+          self.toggleCursorSize();
+        });
+      });
+
+      // Anchor hovering
+      document
+        .querySelectorAll(".slider-images .swiper-slide")
+        .forEach(function (el) {
+          el.addEventListener("mouseover", function () {
+            $(".cursor-custom").addClass("filter");
+            self.cursorEnlarged = true;
+            self.toggleCursorSize();
+          });
+          el.addEventListener("mouseout", function () {
+            $(".cursor-custom").removeClass("filter");
+            self.cursorEnlarged = false;
+            self.toggleCursorSize();
+          });
+        });
+
+      // Click events
+      document.addEventListener("mousedown", function () {
+        self.cursorEnlarged = true;
+        self.toggleCursorSize();
+      });
+      document.addEventListener("mouseup", function () {
+        self.cursorEnlarged = false;
+        self.toggleCursorSize();
+      });
+
+      document.addEventListener("mousemove", function (e) {
+        // Show the cursor
+        self.cursorVisible = true;
+        self.toggleCursorVisibility();
+
+        // Position the dot
+        self.endX = e.pageX;
+        self.endY = e.pageY;
+        self.$dot.style.top = self.endY + "px";
+        self.$dot.style.left = self.endX + "px";
+      });
+
+      // Hide/show cursor
+      document.addEventListener("mouseenter", function (e) {
+        self.cursorVisible = true;
+        self.toggleCursorVisibility();
+        self.$dot.style.opacity = 1;
+        self.$outline.style.opacity = 1;
+      });
+
+      document.addEventListener("mouseleave", function (e) {
+        self.cursorVisible = true;
+        self.toggleCursorVisibility();
+        self.$dot.style.opacity = 0;
+        self.$outline.style.opacity = 0;
+      });
+    },
+
+    animateDotOutline: function () {
+      var self = this;
+
+      self._x += (self.endX - self._x) / self.delay;
+      self._y += (self.endY - self._y) / self.delay;
+      self.$outline.style.top = self._y + "px";
+      self.$outline.style.left = self._x + "px";
+
+      requestAnimationFrame(this.animateDotOutline.bind(self));
+    },
+
+    toggleCursorSize: function () {
+      var self = this;
+
+      if (self.cursorEnlarged) {
+        self.$dot.style.transform = "translate(-50%, -50%) scale(0.75)";
+        self.$outline.style.transform = "translate(-50%, -50%) scale(2)";
+      } else {
+        self.$dot.style.transform = "translate(-50%, -50%) scale(1)";
+        self.$outline.style.transform = "translate(-50%, -50%) scale(1)";
+      }
+    },
+
+    toggleCursorVisibility: function () {
+      var self = this;
+
+      if (self.cursorVisible) {
+        self.$dot.style.opacity = 1;
+        self.$outline.style.opacity = 1;
+      } else {
+        self.$dot.style.opacity = 0;
+        self.$outline.style.opacity = 0;
+      }
+    },
+  };
+  cursorCustom.init();
 }
